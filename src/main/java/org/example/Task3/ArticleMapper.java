85d6f164bf7ed89a48af667a1f20523fcab0a0a7
xHlpNw <aleksashin990@gmail.com>
2025-12-06 22:41:36 +0400

    realized classes

---
+++ src/main/java/org/example/Task3/ArticleMapper.java
@@ -0,0 +1,16 @@
+package org.example.Task3;
+
+public class ArticleMapper {
+    public static SearchResult map(Article article) {
+        return new SearchResult(
+                article.getCode(),
+                article.getTitle(),
+                article.getSupplier().calculateFinalPrice(
+                        article.getBasePrice()
+                ),
+                article.getSupplier().getName(),
+                article.getSupplier().getAddress(),
+                article.getSupplier().getManufacturer().getName()
+        );
+    }
+}
---
+++ src/main/java/org/example/Task3/Manufacturer.java
@@ -0,0 +1,19 @@
+package org.example.Task3;
+
+import java.math.BigDecimal;
+
+public class Manufacturer extends Supplier {
+    public Manufacturer(String tin, String name, String address) {
+        super(tin, name, address);
+    }
+
+    @Override
+    public Manufacturer getManufacturer() {
+        return this;
+    }
+
+    @Override
+    public BigDecimal calculateFinalPrice(BigDecimal basePrice) {
+        return basePrice;
+    }
+}
